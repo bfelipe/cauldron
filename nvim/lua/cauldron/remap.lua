69fd2ea60d4a2e61892df8ca98c9c47b9952034e
Bruno Felipe <cs.felipebm@gmail.com>
2023-10-30 20:16:20 -0300

    add new set of keybindings

--- nvim/lua/cauldron/remap.lua
+++ nvim/lua/cauldron/remap.lua
@@ -7,3 +7,10 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>vx", vim.cmd.Vex)
 vim.keymap.set("n", "<leader>sx", vim.cmd.Sex)
+
+-- Move selected snip with proper indentation
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Replace selected without overiding buffer clipboard
+vim.keymap.set("x", "<leader>p", "\"_dP")

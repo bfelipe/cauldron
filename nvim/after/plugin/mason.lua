0c8d7c2c72e8134ed177575024a7dfe325775038
Bruno Felipe <cs.felipebm@gmail.com>
2023-11-21 16:41:08 -0300

    fix issue where lsp features where absend from mason language server setup

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -1,12 +1,16 @@
-local ms = require("mason").setup()
-local mlsp = require("mason-lspconfig")
+local ms = require('mason').setup()
+local mlsp = require('mason-lspconfig')
+local lsp = require('lsp-zero')
 
 mlsp.setup{
     ensure_installed = {
-        "clangd",
-        "gopls",
-        "lua_ls",
-        "pylsp",
-        "rust_analyzer",
+        'clangd',
+        'gopls',
+        'lua_ls',
+        'pylsp',
+        'rust_analyzer',
+    },
+    handlers = {
+        lsp.default_setup,
     }
 }

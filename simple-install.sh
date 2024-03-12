07ef80bd3e7ccbe0a0699021b4043084011ee7aa
Bruno Felipe <cs.felipebm@gmail.com>
2024-03-12 20:00:09 -0300

    fix relative path for nvim copy

--- simple-install.sh
+++ simple-install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-COMPATIBLE_VERSION="NVIM v0.9.5"
-CURRENT_NVIM_VERSION=$(nvim --version | grep NVIM)
-
 echo "Starting..."
 echo "Preparing Cauldron installation..."
 git pull
 rm -fr ~/.local/share/nvim/ ~/.local/state/nvim ~/.cache/nvim
 echo "- Installing..."
-cp -r nvim ~/.config/
+cp -r ./cauldron/nvim ~/.config/
 echo -e "\U2705 Cauldron has been installed from latest version"

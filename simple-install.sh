d48511b1d28108a4591ce0df6d77dc6df9d4a67d
Bruno Felipe <cs.felipebm@gmail.com>
2024-07-26 11:23:17 -0300

    remove theme config and simple-install tool

--- simple-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting..."
-echo "Preparing Cauldron installation..."
-git pull
-rm -fr ~/.local/share/nvim/ ~/.local/state/nvim ~/.cache/nvim
-echo "- Installing..."
-cp -r ./cauldron/nvim ~/.config/
-echo -e "\U2705 Cauldron has been installed from latest version"
